4d82be1bc7a14eceb9749a24c54041eed18d3bd0
risingisland <risingisland@yahoo.com>
2023-09-28 22:08:42 +0100

    first

---
+++ responsivefilemanager/lang/languages.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'de_DE' => 'Deutsch',
+    'el_GR' => 'ελληνικά',
+    'en_US' => 'English',
+    'es_ES' => 'Español',
+    'fr_FR' => 'Français',
+    'it_IT' => 'Italiano',
+    'ja_JP' => '日本',
+    'nl_NL' => 'Nederlands, Vlaams',
+    'pl_PL' => 'Język polski, polszczyzna',
+    'pt_PT' => 'Português',
+    'ru_RU' => 'Pусский язык',
+    'uk_UK' => 'Yкраїнська мова',
+
+    // source: http://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+];
